ad4e6156c162bd86afeaf24f6e2686e44dbf6d1f
Sam Moore <swmoore84@gmail.com>
2021-05-19 23:19:00 +0900

    add model test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid with a first name, last name, email, and password" do
+    user = User.new(
+      first_name: "Aaron",
+      last_name:  "Sumner",
+      email:      "tester@example.com",
+      password:   "dottle-nouveau-pavilion-tights-furze",
+    )
+    expect(user).to be_valid
+  end
+  it "is invalid without a first name"
+  it "is invalid without a last name"
+  it "is invalid without an email address"
+  it "is invalid with a duplicate email address"
+  it "returns a user's full name as a string"
 end
