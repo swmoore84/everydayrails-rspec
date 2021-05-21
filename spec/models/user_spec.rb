805af41d2cc122f113fc4f263d1ee9207b54ee5b
Sam Moore <swmoore84@gmail.com>
2021-05-21 22:17:55 +0900

    add email testing

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -20,7 +20,26 @@ RSpec.describe User, type: :model do
     user.valid?
     expect(user.errors[:last_name]).to include("can't be blank")
   end
-  it "is invalid without an email address"
-  it "is invalid with a duplicate email address"
+  it "is invalid without an email address" do
+    user = User.new(email: nil)
+    user.valid?
+    expect(user.errors[:email]).to include("can't be blank")
+  end
+  it "is invalid with a duplicate email address" do
+    User.create(
+      first_name:  "Joe",
+      last_name:  "Tester",
+      email:      "tester@example.com",
+      password:   "dottle-nouveau-pavilion-tights-furze",
+    )
+    user = User.new(
+      first_name:  "Jane",
+      last_name:  "Tester",
+      email:      "tester@example.com",
+      password:   "dottle-nouveau-pavilion-tights-furze",
+    )
+    user.valid?
+    expect(user.errors[:email]).to include("has already been taken")
+  end
   it "returns a user's full name as a string"
 end
