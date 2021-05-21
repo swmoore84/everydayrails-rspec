1e0f417d621bb7d073cc987845218e3eba7322bf
Sam Moore <swmoore84@gmail.com>
2021-05-21 22:36:16 +0900

    create project model test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,21 +10,25 @@ RSpec.describe User, type: :model do
     )
     expect(user).to be_valid
   end
+
   it "is invalid without a first name" do
     user = User.new(first_name: nil)
     user.valid?
     expect(user.errors[:first_name]).to include("can't be blank")
   end
+
   it "is invalid without a last name" do
     user = User.new(last_name: nil)
     user.valid?
     expect(user.errors[:last_name]).to include("can't be blank")
   end
+
   it "is invalid without an email address" do
     user = User.new(email: nil)
     user.valid?
     expect(user.errors[:email]).to include("can't be blank")
   end
+
   it "is invalid with a duplicate email address" do
     User.create(
       first_name:  "Joe",
@@ -41,5 +45,6 @@ RSpec.describe User, type: :model do
     user.valid?
     expect(user.errors[:email]).to include("has already been taken")
   end
+
   it "returns a user's full name as a string"
 end
