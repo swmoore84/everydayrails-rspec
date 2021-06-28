35cc8bd5f3d9dabbe3f8935a48c3789c149eb7c2
Sam Moore <swmoore84@gmail.com>
2021-06-28 23:17:27 +0900

    update user tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -27,25 +27,14 @@ RSpec.describe User, type: :model do
     expect(user.errors[:last_name]).to include("can't be blank")
   end
 
-  it "is invalid without an email address" do
-    user = FactoryBot.build(:user, email: nil)
-    user.valid?
-    expect(user.errors[:email]).to include("can't be blank")
+  it "returns a user's full name as a string" do
+    user = FactoryBot.build(:user, first_name: "John", last_name: "Doe")
+    expect(user.name).to eq "John Doe"
   end
 
   it "is invalid with a duplicate email address" do
-    User.create(
-      first_name:  "Joe",
-      last_name:  "Tester",
-      email:      "tester@example.com",
-      password:   "dottle-nouveau-pavilion-tights-furze",
-    )
-    user = User.new(
-      first_name:  "Jane",
-      last_name:  "Tester",
-      email:      "tester@example.com",
-      password:   "dottle-nouveau-pavilion-tights-furze",
-    )
+    FactoryBot.create(:user, email: "aaron@example.com")
+    user = FactoryBot.build(:user, email: "aaron@example.com")
     user.valid?
     expect(user.errors[:email]).to include("has already been taken")
   end
